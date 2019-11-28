05e512474306c1987e6e3300fbe0c5acd19dd731
Harris <harrisdt15f@gmail.com>
2019-11-28 12:34:02 +0800

    update open project with latest v10

--- openproject/db_init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE openproject CHARACTER SET utf8;
-CREATE USER 'openproject'@'%' IDENTIFIED BY 'my_password';
-GRANT ALL PRIVILEGES ON openproject.* TO 'openproject'@'%';
-FLUSH PRIVILEGES;
--- openproject/scripts/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export RAILS_ENV="production"
-source /home/openproject/.profile
-
-# Execute the commands passed to this script
-# e.g. "./env.sh bundle exec rake
-exec "$@"
