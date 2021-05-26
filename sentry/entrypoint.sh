3f64fbdbf8db1faff4ee854f5b668cb8d46d06b3
Harris <harrismt808@gmail.com>
2021-05-26 18:57:15 +0800

    setting gateway for sentry
    sentry db to 15
    
    
    sentry add smtp config

--- sentry/entrypoint.sh
+++ sentry/entrypoint.sh
@@ -1,7 +1,8 @@
-#! / bin / bash
+#!/bin/sh
 
 #config generate-secret-key
 
 #echo 'export SENTRY_SECRET_KEY = "xxxxxxx ......."' >> ~ / .bashrc
 #source ~ / .bashrc
 #echo $ SENTRY_SECRET_KEY
+sentry run worker & sentry run cron &
