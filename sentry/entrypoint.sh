28e324f6d7f56155e252cc5a6b3078fdb06b8772
Harris <harrismt808@gmail.com>
2021-05-28 10:34:58 +0800

    solve relay errors

--- sentry/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#config generate-secret-key
-
-#echo 'export SENTRY_SECRET_KEY = "xxxxxxx ......."' >> ~ / .bashrc
-#source ~ / .bashrc
-#echo $ SENTRY_SECRET_KEY
-sentry run worker & sentry run cron &
